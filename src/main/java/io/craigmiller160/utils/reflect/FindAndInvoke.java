7efc4d575df1f65d18c57c197bad7c1561665893
craigmiller160 <craigmiller160@gmail.com>
2016-05-20 16:27:43 -0400

    Fixed bad comment

--- src/main/java/io/craigmiller160/utils/reflect/FindAndInvoke.java
+++ src/main/java/io/craigmiller160/utils/reflect/FindAndInvoke.java
@@ -31,7 +31,7 @@ import java.util.List;
 public class FindAndInvoke {
 
 	//TODO Multiple methods with the same name and different params can cause issues with newParams being null or having null values, it makes it tougher to find a match because of ambiguity. This should be tested for and an 
-exception thrown.
+//exception thrown.
 
 
     /**
